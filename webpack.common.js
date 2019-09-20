785d08dd08935bfa7667d88c4c40a6a88272d3d9
Takumi Adachi <takumiadachi@gmail.com>
2019-09-20 16:49:22 -0700

    Initial commit.

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const webpack = require("webpack");
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const srcDir = './src/';
+
+module.exports = {
+  entry: {
+    "page-eater": path.join(__dirname, srcDir + 'page-eater.ts'),
+    background: path.join(__dirname, srcDir + 'background.ts')
+  },
+  output: {
+    path: path.join(__dirname, './modify-page/'),
+    filename: '[name].js'
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: "initial"
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  plugins: [
+    // exclude locale files in moment
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new CopyPlugin([{
+      from: "manifest.json",
+      to: './'
+    }], {
+      context: './'
+    }),
+  ]
+};
