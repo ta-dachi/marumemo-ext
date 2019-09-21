5421acac0357f326f5534b3bac9d4a22c80ca160
Takumi Adachi <takumiadachi@gmail.com>
2019-09-21 10:44:13 -0700

    Can eat pages using context menu.

--- src/page-eater.ts
+++ src/page-eater.ts
@@ -3,3 +3,5 @@ document.body.textContent = "";
 var header = document.createElement("h1");
 header.textContent = "This page has been eaten";
 document.body.appendChild(header);
+
+console.log("page-eater.js");
